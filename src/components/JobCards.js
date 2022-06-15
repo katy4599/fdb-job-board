c1795b9976751d7f30e2479e8d032df6b8763508
katy4599 <katy.millard@gmail.com>
2022-06-15 15:55:10 -0500

    first contribution

--- src/components/JobCards.js
+++ src/components/JobCards.js
@@ -1,9 +1,16 @@
 import React from "react";
 
-export default function JobCards() {
+export default function JobCards(props) {
+    console.log(props)
     return (
-        <div className="jobCard">
-            <h1>Here's a great job!</h1>
+        <div>
+            <div className="jobCard">
+                <a href={props.url} className="jobTitle">
+                    <h1 >{props.title}</h1>
+                </a>
+                    <h2 className='comp-loc'>{props.company} * {props.location}</h2> 
+                    <h4 className='description'>{props.description}</h4> 
+            </div>
         </div>
     )
 }
